d6b56f29b0b1c81435d605686200c41388ce487d
thezzisu <thezzisu@gmail.com>
2022-07-02 23:03:18 +0800

    fuck

--- shit.py
+++ shit.py
@@ -2,5 +2,5 @@ import subprocess
 
 subprocess.run(['ln', '-s', "/flag", "flag0"])
 
-for i in range(256):
+for i in range(1024):
   subprocess.run(['ln', '-s', "flag{}".format(i), "flag{}".format(i+1)])
